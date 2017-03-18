29f1e96669bb58645285c49e19f10d2d2bbd9288
Harry Chasparis <zchas001@ucr.edu>
2017-03-18 15:22:53 -0700

    Add files via upload

--- scripts/createInputFiles/createFilesScript.sh
+++ scripts/createInputFiles/createFilesScript.sh
@@ -4,6 +4,8 @@ echo "Create new files"
 inputfile='all_nodes'
 outNum='536480353 1072960705 1609441058 2145921410'
 
+membudget='10 100 250 500'
+
 #inputfile='tweets-2014-06-14'
 #outNum='2626851 5253702 7880552'
 
@@ -18,6 +20,15 @@ do
 	name="0$temp$inputfile"
 	echo $name
 	shuf -n $i $inputfile > $name
+
+	for i in {1..3}
+	do
+		for j in $membudget
+		do
+			./bin/spark-submit  --class "test" mytest-1.0.jar $i $j $name |& tee -a outDifInputSizes
+		done
+	done
+
 	k=`expr $k + $one`
 done
 
